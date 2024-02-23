4161e00de88c8cf322342913211c7c6e3cf28be4
michelruizfuentes <mruiizfuentes@gmail.com>
2024-02-23 13:54:59 -0500

    # ADDING INSTALL_R SCRIPT

---
+++ INSTALL_ALL.R
@@ -0,0 +1,12 @@
+# if renv not installed, install it
+if(!require("renv")) install.packages("renv")
+
+# collect all dependencies in current directory
+depend <- renv::dependencies(
+  path = ".")$Package |> unique()
+
+# check which dependencies are not installed
+packages_not_installed <- depend[!depend %in% installed.packages()]
+
+# install needed dependencies
+install.packages(packages_not_installed)
